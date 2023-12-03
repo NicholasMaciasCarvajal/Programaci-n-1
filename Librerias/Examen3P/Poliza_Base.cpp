564efcdbdbda334f83f570657240cae441111ad1
NicholasMaciasCarvajal <nicholas.macias@uniat.edu.mx>
2023-12-03 17:38:46 -0600

    Version 0.2 Completación del codigo sobre las polizas, nose si esta correcto no me deja correrlo.

--- Librerias/Examen3P/Poliza_Base.cpp
+++ Librerias/Examen3P/Poliza_Base.cpp
@@ -4,7 +4,7 @@
 //creación de la clase base
 class Base {
 public:
-    Base(std::string nombre, std::string curp, int edad, int ano, float deducible, float prestamo_max){}
+    Base(std::string nombre, std::string curp, int * edad, int * ano, float deducible, float prestamo_max){}
     //Uso de sets
     void setNombre(std::string nombre){
         this->nombre = nombre;
@@ -13,7 +13,7 @@ public:
         this->curp = curp;
     };
     void setEdad(int edad){
-        this->edad = edad;
+        this->edad = 2023 - * ano;
     };
     void setAno(int ano){
         this->ano = ano;
@@ -51,20 +51,117 @@ private:
     float deducible;
     float prestamo_max;
 protected:
-    friend class Poliza_Casa;
+    friend class PolizaCasa;
     std::string getBase() {
         return this -> nombre;
-        return this -> curp;
-        return this -> edad;
-        return this -> ano;
+        return this -> prestamo_max;
+        return this -> deducible;
     }
-    friend class Poliza_Vida;
-
-    friend class Poliza_Carro;
-
+    friend class PolizaVida;
+    std::string getBase() {
+        return this -> nombre;
+        return this -> edad;
+        return this -> deducible;
+        return this -> prestamo_max;
+    }
+    friend class PolizaCarro;
+    std::string getBase() {
+        return this -> nombre;
+        return this -> deducible;
+        return this -> prestamo_max;
+    }
 };
 //creacion de la clase poliza de casa
-
+class PolizaCasa : public Base{
+public:
+    PolizaCasa(int fconstruccion, std::string direccion, float tam) : Base(nombre, deducible, prestamo_max){}
+    //uso de sets
+    void setFconstruccion(int fconstruccion){
+        this->fconstruccion = fconstruccion;
+    };
+    void setTam(float tam){
+        this->tam = tam;
+    };
+    void setDireccion(std::string direccion){
+        this->direccion = direccion;
+    };
+    //uso de gets
+    std::string getDireccion() {
+        return this -> direccion;
+    }
+    int getFconstruccion() {
+        return this -> fconstruccion;
+    }
+    float getTam() {
+        return this -> tam;
+    }
+private:
+    std::string direccion;
+    int fconstruccion;
+    float tam;
+};
 //creacion de la clase poliza de vida
-
+class PolizaVida : public Base {
+public:
+    PolizaVida (bool enfermedades, std::string tiposangre) : Base(nombre, edad, deducible, prestamo_max){}
+    //uso de sets
+    void setEnfermedades(bool enfermedades){
+        this->enfermedades = enfermedades;
+    }
+    void setTiposangre(std::string tiposangre){
+        this->tiposangre = tiposangre;
+    }
+    //uso de gets
+    bool getEnfermedades(){
+        return this -> enfermedades;
+    }
+    std::string getTiposangre(){
+        return this -> tiposangre;
+    }
+private:
+    std::string tiposangre;
+    bool enfermedades;
+};
 //creacion de la clase poliza de carro
+class PolizaCarro : public Base {
+public:
+    PolizaCarro (std::string modelo, std::string marca, int ano2, float kilometraje, std::string numdeserie) : Base(nombre, deducible, prestamo_max){}
+    //uso de sets
+    void setModelo(std::string modelo){
+        this->modelo = modelo;
+    }
+    void setMarca(std::string marca){
+        this->marca = marca;
+    }
+    void setNumdeserie(std::string numdeserie){
+        this->numdeserie = numdeserie;
+    }
+    void setAno2(int ano2){
+        this->ano2 = ano2;
+    }
+    void setkilometraje(float kilometraje){
+        this->kilometraje = kilometraje;
+    }
+    //uso de gets
+    std::string getModelo(){
+        return this -> modelo;
+    }
+    std::string getMarca(){
+        return this -> marca;
+    }
+    std::string getNumdeserie(){
+        return this -> numdeserie;
+    }
+    int getAno2(){
+        return this -> ano2;
+    }
+    float getKilometraje(){
+        return this -> Kilometraje;
+    }
+private:
+    std::string modelo;
+    std::string marca;
+    std::string numdeserie;
+    int ano2;
+    float kilometraje;
+};
--- Librerias/Examen3P/main.cpp
+++ Librerias/Examen3P/main.cpp
@@ -1,3 +1,38 @@
-//
-// Created by Alumnos on 30/11/2023.
-//
+#include <iostream>
+#include <string>
+
+int main(){
+    Base Diego("Diego","AWWS20123012AS223GWS", edad, 2000, 10000, 1000000);
+    std::cout << Diego.getNombre() << std::endl;
+    std::cout << Diego.getCurp() << std::endl;
+    std::cout << Diego.getEdad() << std::endl;
+    std::cout << Diego.getAno() << std::endl;
+    std::cout << Diego.getDeducible() << std::endl;
+    std::cout << Diego.getPrestamo_Max() << std::endl;
+
+    PolizaCasa Casa("Diego", 10000, 1000000, 2020, "Lopez Mateos", 25.2);
+    std::cout << Casa.getNombre() << std::endl;
+    std::cout << Casa.getDeducible() << std::endl;
+    std::cout << Casa.getPrestamo_Max() << std::endl;
+    std::cout << Casa.getFconstruccion() << std::endl;
+    std::cout << Casa.getDireccion() << std::endl;
+    std::cout << Casa.getTam() << std::endl;
+
+    PolizaVida Vida("Diego", edad, 10000, 1000000, 0, "A");
+    std::cout << Vida.getNombre() << std::endl;
+    std::cout << Vida.getEdad() << std::endl;
+    std::cout << Vida.getDeducible() << std::endl;
+    std::cout << Vida.getPrestamo_Max() << std::endl;
+    std::cout << Vida.getEnfermedades() << std::endl;
+    std::cout << Vida.getTiposangre() << std::endl;
+
+    PolizaCarro Carro("Diego", 10000, 1000000, "01294ofn", "Toyota", 2020, 0, "0000001");
+    std::cout << Carro.getNombre() << std::endl;
+    std::cout << Carro.getDeducible() << std::endl;
+    std::cout << Carro.getPrestamo_Max() << std::endl;
+    std::cout << Carro.getModelo() << std::endl;
+    std::cout << Carro.getMarca() << std::endl;
+    std::cout << Carro.getAno2() << std::endl;
+    std::cout << Carro.getKilometraje() << std::endl;
+    std::cout << Carro.getNumdeserie() << std::endl;
+}
